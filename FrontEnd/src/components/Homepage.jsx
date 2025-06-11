4b7e4735c52365253f8a33cd4a3e644a7615e118
Sachin Mahapatra <sachinmahapatra858@gmail.com>
2025-06-11 10:40:01 +0530

    title change

--- FrontEnd/src/components/Homepage.jsx
+++ FrontEnd/src/components/Homepage.jsx
@@ -94,7 +94,7 @@ function Homepage() {
         }}
       >
         {/* Background overlay with medical items */}
-        <div className="absolute inset-0 bg-gradient-to-r from-blue-900/95 to-blue-800/90 z-0">
+        <div className="absolute inset-0 bg-gradient-to-r from-blue-900/95 to-blue-800/60 z-0">
           <div className="absolute inset-0 opacity-10">
             <div className="absolute top-10 left-10 w-32 h-32 bg-white/20 rounded-full blur-2xl"></div>
             <div className="absolute bottom-20 right-20 w-40 h-40 bg-red-500/20 rounded-full blur-2xl"></div>
